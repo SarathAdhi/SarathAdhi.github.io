c35077589bd5f3cfaae9d5169c0138d24fbaf3e8
SarathAdhi <s.sarathadhithya@gmail.com>
2023-06-18 15:07:36 +0530

    gh pages

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    loader: "akamai",
+    path: "",
     domains: ["avatars.githubusercontent.com", "seeklogo.com", "cdn.sanity.io"],
   },
 };
