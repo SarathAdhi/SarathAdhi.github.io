678344bcac6c8732915525cb5f8445a861bd3f81
SarathAdhi <s.sarathadhithya@gmail.com>
2023-06-18 15:11:11 +0530

    minor changes

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
     path: "",
     domains: ["avatars.githubusercontent.com", "seeklogo.com", "cdn.sanity.io"],
   },
+  env: {
+    SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+    SANITY_TOKEN_ID: process.env.SANITY_TOKEN_ID,
+    SERVER_URL: process.env.SERVER_URL,
+  },
 };
 
 module.exports = nextConfig;
